d60788f4833b0df03210d6a5c47c50333d49b355
Marek Kopli <marek.kopli@tptlive.ee>
2022-09-21 14:07:47 +0300

    items

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const PurgecssPlugin = require('purgecss-webpack-plugin');
 const path = require('path');
 const glob = require('glob');
-
-let htmlPlugins = [];
-
-let files = glob.sync('./src/views/*.twig');
-files.forEach(file => {
-  let htmlPlugin = new HtmlWebpackPlugin({
-    filename: file.split('/').at(-1).replace('twig','html'),
-    template: file
-  });
-  htmlPlugins.push(htmlPlugin);
-});
+const { VueLoaderPlugin } = require('vue-loader')
 
 
-const PATHS = {
-  src: path.join(__dirname, 'src/views')
-}
 
 module.exports = {
   entry: './src/index.js',
@@ -40,6 +26,14 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.vue$/i,
+        use: ["vue-loader"],
+      },
+      {
         test: /\.twig$/,
         use: {
           loader: 'twig-loader',
@@ -47,18 +41,19 @@ module.exports = {
               // See options section below
           },
         }
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
+      }
     ],
   },
   plugins: [
-    ...htmlPlugins,
-    new MiniCssExtractPlugin(),
-    new PurgecssPlugin.PurgeCSSPlugin({
-      paths: glob.sync(`${PATHS.src}/**/*`,  { nodir: true }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
     }),
+    new MiniCssExtractPlugin(),
+    new VueLoaderPlugin(),
   ],
+  resolve: {
+    alias: {
+      vue:'vue/dist/vue.esm-bundler.js'
+    }
+  }
 };
